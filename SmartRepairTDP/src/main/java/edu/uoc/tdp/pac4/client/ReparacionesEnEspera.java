77ee7104f963d7a9811fea1e667814dc28358055
franog <francesc.olive@hotmail.com>
2013-05-06 20:20:27 +0200

    Primera subida

--- SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/client/ReparacionesEnEspera.java
+++ SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/client/ReparacionesEnEspera.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class ReparacionesEnEspera extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tblReparacionesEspera;
+	private JTable tblPiezasAsignadas;
 
 	/**
 	 * Launch the application.
@@ -32,11 +40,78 @@ public class ReparacionesEnEspera extends JFrame {
 	 */
 	public ReparacionesEnEspera() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 316);
 		contentPane = new JPanel();
+		contentPane.setToolTipText("aaaaaa");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		tblReparacionesEspera = new JTable();
+		
+		JLabel lblReparacionesEspera = new JLabel("Reparaciones en Espera");
+		
+		JLabel lblPiezasAsignadas = new JLabel("Piezas Asignadas");
+		
+		tblPiezasAsignadas = new JTable();
+		
+		JButton btnAnadir = new JButton("Añadir");
+		
+		JButton btnModificar = new JButton("Modificar");
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		
+		JButton btnAsignarMecanico = new JButton("Asignar Mecánico");
+		
+		JButton btnSalir = new JButton("Salir");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(lblReparacionesEspera)
+								.addComponent(lblPiezasAsignadas)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(tblPiezasAsignadas, GroupLayout.PREFERRED_SIZE, 315, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(btnModificar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(btnEliminar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(btnAnadir, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+								.addComponent(tblReparacionesEspera, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(119)
+							.addComponent(btnAsignarMecanico)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnSalir)))
+					.addContainerGap(18, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(4)
+					.addComponent(lblReparacionesEspera)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(tblReparacionesEspera, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE)
+					.addGap(27)
+					.addComponent(lblPiezasAsignadas)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnAnadir)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnModificar)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnEliminar))
+						.addComponent(tblPiezasAsignadas, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAsignarMecanico)
+						.addComponent(btnSalir))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
