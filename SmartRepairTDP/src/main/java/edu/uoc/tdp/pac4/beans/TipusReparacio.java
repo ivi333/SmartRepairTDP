120a43c125545f8e7658261aad06d4ede81a769e
SaraUOC <sqg_uoc@hotmail.com>
2013-05-12 22:25:22 +0200

    Actualización clase TipusReparacio

--- SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/beans/TipusReparacio.java
+++ SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/beans/TipusReparacio.java
@@ -1,6 +1,5 @@
 package edu.uoc.tdp.pac4.beans;
 
 public enum TipusReparacio {
-	EnCurs, EnEspera, Rebudes, Rebutjades, Finalitzades, Totes, PerNomClient,
-	PerNomDeMecanic, Prueba
+	EnCurs, EnEspera, Rebudes,Finalitzades, Totes
 }
