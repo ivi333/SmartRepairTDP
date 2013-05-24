9dd7d730b9475bc94e68f128a2edc9d9f363afda
franog <francesc.olive@hotmail.com>
2013-05-25 01:17:37 +0200

    Deshacer checkouts

--- SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/server/Servidor.java
+++ SmartRepairTDP/src/main/java/edu/uoc/tdp/pac4/server/Servidor.java
@@ -59,9 +59,7 @@ public class Servidor extends javax.swing.JFrame {
 	
 	private void initRemoteObjects () {
 		try {
-			
-				gestorAdmin = new GestorAdministracionImpl();
-		
+			gestorAdmin = new GestorAdministracionImpl ();
 			gestorConex = new GestorConexionImpl();
 			gestorEstad = new GestorEstadisticaImpl();
 			gestorRepar = new GestorReparacionImpl();
@@ -85,7 +83,6 @@ public class Servidor extends javax.swing.JFrame {
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		addWindowListener(new java.awt.event.WindowAdapter() {
-			@Override
 			public void windowClosing(java.awt.event.WindowEvent evt) {
 				formWindowClosing(evt);
 			}
